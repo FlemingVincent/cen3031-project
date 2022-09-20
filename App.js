3a46216c041ed0eef5a9b9437ac148a493086700
flemingvincent <flemingvincent@ufl.edu>
2022-09-20 11:41:11 -0400

    set up path alias and tailwind for styling

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import tw from 'src/lib/tailwind';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`flex-1 bg-[#fff] items-center justify-center`}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// This is how React Native styles screens, think of the stylesheet as a .css file.
+// Instead we are going to use Tailwind, look at the View above to see how I am styling the same way.
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
